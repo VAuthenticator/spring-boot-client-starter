94415d05fa38c086adcd102cc68691d38774d774
mrflick72 <valerio.vaudi@gmail.com>
2022-12-12 21:03:20 +0100

    make VAuthenticatorOidcUserService as general available

--- src/main/java/it/valeriovaudi/vauthenticator/security/clientsecuritystarter/user/VAuthenticatorUserConfig.java
+++ src/main/java/it/valeriovaudi/vauthenticator/security/clientsecuritystarter/user/VAuthenticatorUserConfig.java
@@ -1,11 +1,18 @@
 package it.valeriovaudi.vauthenticator.security.clientsecuritystarter.user;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 
 @AutoConfiguration
 public class VAuthenticatorUserConfig {
+
+    @Bean
+    public VAuthenticatorOidcUserService vAuthenticatorOidcUserService(@Value("${vauthenticator.authorities-claim-name:authorities") String authorize){
+        OidcUserService oidcUserService = new OidcUserService();
+        return new VAuthenticatorOidcUserService(oidcUserService, authorize);
+    }
 
     @Bean
     public VAuthenticatorUserNameResolver vAuthenticatorUserNameResolver() {
