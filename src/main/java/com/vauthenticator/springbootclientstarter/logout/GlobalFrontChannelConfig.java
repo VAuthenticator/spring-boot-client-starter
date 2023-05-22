8f8aa5d28417dbbf6711eceaedfe47d6f9a61b62
mrflick72 <valerio.vaudi@gmail.com>
2023-05-22 23:50:13 +0200

    no discovery mode

--- src/main/java/com/vauthenticator/springbootclientstarter/logout/GlobalFrontChannelConfig.java
+++ src/main/java/com/vauthenticator/springbootclientstarter/logout/GlobalFrontChannelConfig.java
@@ -12,17 +12,6 @@ import org.springframework.web.client.RestTemplate;
 public class GlobalFrontChannelConfig {
 
     @Bean("globalFrontChannelLogoutProvider")
-    @ConditionalOnProperty(value = {"postLogoutRedirectUri", "auth.oidcIss","preferDiscovery"})
-    public GlobalFrontChannelLogoutProvider globalFrontChannelLogoutProviderWithDiscovery(@Value("${postLogoutRedirectUri}") String postLogoutRedirectUri,
-                                                                             @Value("${auth.oidcIss}") String oidConnectDiscoveryEndPoint) {
-        return new GlobalFrontChannelLogoutProvider(postLogoutRedirectUri,
-                oidConnectDiscoveryEndPoint + "/.well-known/openid-configuration",
-                null,
-                new RestTemplate());
-    }
-
-    @Bean("globalFrontChannelLogoutProvider")
-    @ConditionalOnProperty(value = {"endSessionWithoutDiscovery"})
     public GlobalFrontChannelLogoutProvider globalFrontChannelLogoutProviderWithoutDiscovery(@Value("${postLogoutRedirectUri:''}") String postLogoutRedirectUri,
                                                                                              @Value("${oidcEndSessionUrl}") String oidcEndSessionUrl) {
         return new GlobalFrontChannelLogoutProvider(postLogoutRedirectUri,
